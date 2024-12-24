55ff9d20f09ca9bd5f7b642e8703e4bb0700ed83
hemant <hemantnegi708@gmail.com>
2024-12-24 16:35:07 +0530

    basic routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import Link from "next/link";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -9,13 +9,23 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="flex p-14 justify-between">
-          <h1 className="font-bold text-3xl">Hemant Negi</h1>
-          <div className="flex space-x-10">
-            <a>ABOUT ME</a>
-            <a>PROJECTS </a>
-            <a>RESUME </a>
-            <a>CONTACT </a>
-          </div>
+          <h1 className="font-bold text-3xl">
+            <Link href="/">Hemant Negi</Link>
+          </h1>
+          <ul className="flex space-x-10">
+            <li>
+              <Link href="/about">ABOUT ME</Link>
+            </li>
+            <li>
+              <Link href="/resume">RESUME</Link>
+            </li>
+            <li>
+              <Link href="/projects">PROJECTS</Link>
+            </li>
+            <li>
+              <Link href="/contact">CONTACT</Link>
+            </li>
+          </ul>
         </div>
         {children}
       </body>
