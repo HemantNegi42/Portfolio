21f2dc2a77350ac30cb4582b0378109ea5535a94
hemant <hemantnegi708@gmail.com>
2024-12-28 19:43:42 +0530

    project and contact layout

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,35 @@
 export default function ContactPage() {
-  return <h1>contact Page</h1>;
+  return (
+    <div className="flex flex-col justify-center items-center bg-[#E6DACE]">
+      <h1 className="text-5xl font-bold py-20">Let's Talk</h1>
+      <div className="w-[50%] px-10 py-20 bg-white shadow-2xl rounded-lg grid lg:grid-cols-2 my-8 gap-3">
+        <div>
+          <h1>First Name* </h1>
+          <input className="p-3 mt-4 border w-full border-b-sky-400"></input>
+        </div>
+        <div>
+          <h1>Last Name* </h1>
+          <input
+            className="p-3 mt-4 border w-full border-b-sky-400"
+            type="email"
+          ></input>
+        </div>
+        <div>
+          <h1>Email* </h1>
+          <input className="p-3 mt-4 border w-full border-b-sky-400"></input>
+        </div>
+        <div>
+          <h1>Subject </h1>
+          <input className="p-3 mt-4 border w-full border-b-sky-400"></input>
+        </div>
+        <div>
+          <h1>Message </h1>
+          <input
+            className="p-3 mt-4 border w-full border-b-sky-400"
+            type=""
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,36 @@
+import Image from "next/image";
+import profile from "../../public/Profile.png";
 export default function Projects() {
-  return <h1>All Project here</h1>;
+  return (
+    <div className="flex flex-col justify-center items-center bg-[#E6DACE]">
+      <h1 className="text-5xl font-bold py-20">Projects</h1>
+
+      <div className="w-[50%]">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+          voluptatem libero a, aliquam non voluptatum commodi quas sequi
+          praesentium quibusdam fugiat odit harum nulla molestiae doloribus quod
+          similique ad facilis.
+        </p>
+        <div className="bg-white shadow-2xl rounded-lg flex justify-between my-8 flex-col lg:flex-row">
+          <div className="px-10 py-20 w-[60%] flex flex-col">
+            <div>
+              <h1 className="text-[#0050FF] text-xl font-bold">Project1</h1>
+              <p className="text-md font-semibold ">Role Title</p>
+              <p className="text-sm font-extralight">TechStack</p>
+            </div>
+            <p className="my-10">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+              voluptas a itaque ab voluptatum unde architecto quas optio quod
+              maiores. Illum modi sapiente fugiat similique consectetur laborum
+              doloremque velit mollitia!
+            </p>
+          </div>
+          <div className="lg:w-1/2 rounded-lg">
+            <Image alt="project" src={profile} className="h-full w-full" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
